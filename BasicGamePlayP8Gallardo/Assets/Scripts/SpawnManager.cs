ab2f86994b6c13c50fec37aa33dc5c1c3cdb6322
slickw <723012@student.dusd.net>
2025-04-28 14:53:58 -0700

    almot done

--- BasicGamePlayP8Gallardo/Assets/Scripts/SpawnManager.cs
+++ BasicGamePlayP8Gallardo/Assets/Scripts/SpawnManager.cs
@@ -7,10 +7,12 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 4;
+    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
@@ -18,9 +20,17 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
+            SpawnRandomAnimal();
+        }
+
+
+        void SpawnRandomAnimal()
+        {
             Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
             int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation); }
-       }
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        }
     }
+}
+
 
